ccd3bbf533d9811b57608f3e8e583ef56370bf43
hsiangawang <b03901098@ntu.edu.tw>
2020-09-15 01:19:43 -0500

    Authentication v1.0 and need refactoring

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express')
-const app = express();
 const mongoose = require('mongoose');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
 const LocalStrategy = require('passport-local');
 const bodyParser = require("body-parser");
+const User = require('./models/user')
 
 const port = 3000;
+
+var app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 // Connect to mongoDB cluster
-const mongo_connection = "mongodb+srv://Adam:XDBoost@cluster0.clmro.mongodb.net/user_auth?retryWrites=true&w=majority"
+const mongo_connection = 'mongodb+srv://Adam:XDBoost@cluster0.clmro.mongodb.net/user_auth?retryWrites=true&w=majority'
 mongoose.connect(mongo_connection, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -16,18 +20,71 @@ mongoose.connect(mongo_connection, {
 .then(() => console.log('Connected to DB!'))
 .catch(error => console.log(error.message));
 
+app.use(require("express-session")({
+    secret: "XDBoost",
+    resave: false,
+    saveUnitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-    res.render("home");
+app.get('/', (req, res) => {
+    res.render('home');
 });
 
-app.get('/secret', (req, res) => {
-    res.render("secret");
+app.get('/secret', isLoggedIn, (req, res) => {
+    res.render('secret');
 })
 
+// Auth Routes
+
+//show sign up form
+app.get("/register", function(req, res){
+    res.render("register"); 
+ });
+ //handling user sign up
+ app.post("/register", function(req, res){
+     User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+         if(err){
+             console.log(err);
+             return res.render('register');
+         }
+         passport.authenticate("local")(req, res, function(){
+            res.redirect("/secret");
+         });
+     });
+ });
+
+ // LOGIN ROUTES
+//render login form
+app.get("/login", function(req, res){
+    res.render("login"); 
+ });
+ //login logic
+ //middleware
+ app.post("/login", passport.authenticate("local", {
+     successRedirect: "/secret",
+     failureRedirect: "/login"
+ }) ,function(req, res){
+ });
+ 
+ app.get("/logout", function(req, res){
+     req.logout();
+     res.redirect("/");
+ });
+ 
+ 
+ function isLoggedIn(req, res, next){
+     if(req.isAuthenticated()){
+         return next();
+     }
+     res.redirect("/login");
+ }
 
 app.listen(port, ()=> {
-    console.log("Start Connection");
+    console.log('Start Connection');
 });
